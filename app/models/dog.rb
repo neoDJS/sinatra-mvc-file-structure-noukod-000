4028b5dfd00f6622c8cd20f234f1d2e38b2d2310
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-14 12:18:01 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  @@all = []
+  attr_accessor :name, :breed, :age
+
+  def initialize(name, breed, age)
+    self.name = name
+    self.breed = breed
+    self.age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all.dup.freeze
+  end
+end
